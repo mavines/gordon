364d91301edafd2ba6685e2f944af6f8bcbd8e31
Mason Vines <Mason.Vines@gmail.com>
2020-10-01 13:47:15 -0400

    Rendering hexes.

--- src/train_graph/core.clj
+++ src/train_graph/core.clj
@@ -8,23 +8,23 @@
                     :links [[0 5]]}])
 
 (def double-loop-track [{:id "a" :row 0 :col 0
-                         :links [[5 4]]}
+                         :links [[:se :e]]}
                         {:id "b" :row 0 :col 1
-                         :links [[1 4]]}
+                         :links [[:w :e]]}
                         {:id "c" :row 0 :col 2
-                         :links [[1 5] [1 3]]}
+                         :links [[:w :se] [:w :sw]]}
                         {:id "d" :row 1 :col 0
-                         :links [[0 5]]}
+                         :links [[:nw :se]]}
                         {:id "e" :row 1 :col 1
-                         :links [[2 3]]}
+                         :links [[:ne :sw]]}
                         {:id "f" :row 1 :col 2
-                         :links [[0 3]]}
+                         :links [[:nw :sw]]}
                         {:id "g" :row 2 :col 0
                          :links []}
                         {:id "h" :row 2 :col 1
-                         :links [[0 4] [0 2]]}
+                         :links [[:nw :e] [:nw :ne]]}
                         {:id "i" :row 2 :col 2
-                         :links [[1 2]]}])
+                         :links [[:w :ne]]}])
 
 (defn render [track]
   (track/render track)
@@ -51,7 +51,7 @@
   :setup setup
   ; update-state is called on each iteration before draw-state.
   ;:update update-state
-  :draw render
+  :draw track/render
   :features [:keep-on-top]
   ; This sketch uses functional-mode middleware.
   ; Check quil wiki for more info about middlewares and particularly
--- src/train_graph/track.clj
+++ src/train_graph/track.clj
@@ -1,39 +1,43 @@
-(ns trains.track)
+(ns train-graph.track
+  (:require [quil.core :as q]))
 
-(defonce base-hex [[87 0] [174 50]
-                   [174 150] [87 200]
-                   [0 150] [0 50]
-                   [87 0]])
+(def hex-lines [[[87 0] [174 50]]
+                [[174 50] [174 150]]
+                [[174 150] [87 200]]
+                [[87 200] [0 150]]
+                [[0 150] [0 50]]
+                [[0 50] [87 0]]])
 
 (def hex-width 175)
 (def hex-height 150)
 (def hex-center [87 100])
-(def link-positions {0 [40 20]
-                     1 [0 100]
-                     2 [135 20]
-                     3 [40 180]
-                     4 [175 100]
-                     5 [135 180]})
+(def link-positions {:nw [40 20]
+                     :w [0 100]
+                     :ne [135 20]
+                     :sw [40 180]
+                     :e [175 100]
+                     :se [135 180]})
 
-(defn update-x [point row col]
-  (let [x (first point)        
-        half-offset (if (odd? row)
+(defn update-x [line row col]
+  (let [half-offset (if (odd? row)
                       (/ hex-width 2)
                       0)
         offset (* col hex-width)]
-    (update point 0 #(+ x offset half-offset))))
+    (-> line
+        (update-in [0 0] + offset half-offset)
+        (update-in [1 0] + offset half-offset))))
 
-(defn update-y [point row]
-  (let [y (last point)
-        offset (* row hex-height)]
-    (update point 1 #(+ y offset))))
+(defn update-y [line row]
+  (let [offset (* row hex-height)]
+    (-> line
+        (update-in [0 1] + offset)
+        (update-in [1 1] + offset))))
 
 (defn hex [tile]
   (let [{:keys [row col]} tile]
-    (->> base-hex
+    (->> hex-lines
          (map #(update-x % row col))
          (map #(update-y % row)))))
-
 
 
 (defn link-points [link]
@@ -45,20 +49,14 @@
             hex-center)))
 
 (defn render-link [row col link]
-  ^{:key link}
-  [:polyline {:points (->> (link-points link)
-                           (map #(update-x % row col))
-                           (map #(update-y % row)))
-              :stroke "red" :stroke-width "5pt"}])
+  )
 
 (defn render-tile [tile]
-  ^{:key (:id tile)}
-  [:g
-   [:polyline {:points (hex tile)
-               :fill "green" :stroke "black" :stroke-width "5pt"}]
-   (map #(render-link (:row tile) (:col tile) %) (:links tile))])
+  (doall (map #(apply q/line %) (hex tile))))
 
 (defn render [track]
-  [:div 
-   [:svg {:width 300 :height 300 :viewBox [0 0 500 600]}
-    (map render-tile track)]])
+  (q/background 255 255 255)
+  (q/stroke-weight 5)
+  (q/stroke 0 0 0)
+  (doall (map render-tile track)))
+
