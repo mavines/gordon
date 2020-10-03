8725938319afe76b5a3b6ae9ecc6149e1267efde
Mason Vines <Mason.Vines@gmail.com>
2020-10-02 21:04:22 -0400

    Added a figure eight.

--- src/gordon/data.clj
+++ src/gordon/data.clj
@@ -4,7 +4,7 @@
   [{:id "a" :row 0 :col 0
     :links [[0 5]]}])
 
-(def stuck-track
+(def stuck
   [{:id "a" :row 0 :col 0
     :links [[:se :e]]}
    {:id "b" :row 0 :col 1
@@ -24,7 +24,7 @@
    {:id "i" :row 2 :col 2
     :links [[:w :ne]]}])
 
-(def double-loop-track
+(def double-loop
   [{:id "a" :row 0 :col 0
     :links [[:se :e]]}
    {:id "b" :row 0 :col 1
@@ -43,3 +43,33 @@
     :links [[:nw :e] [:nw :ne]]}
    {:id "i" :row 2 :col 2
     :links [[:w :ne]]}])
+
+(def figure-eight
+  [{:id "a" :row 0 :col 0
+    :links [[:se :e]]}
+   {:id "b" :row 0 :col 1
+    :links [[:w :se]]}
+   {:id "c" :row 0 :col 2
+    :links []}
+   {:id "d" :row 1 :col 0
+    :links [[:sw :nw]]}
+   {:id "e" :row 1 :col 1
+    :links [[:sw :e] [:nw :e]]}
+   {:id "f" :row 1 :col 2
+    :links [[:w :ne] [:w :se]]}
+   {:id "g" :row 2 :col 0
+    :links [[:e :ne]]}
+   {:id "h" :row 2 :col 1
+    :links [[:w :ne]]}
+   {:id "i" :row 2 :col 2
+    :links []}
+   {:id "j" :row 0 :col 3
+    :links [[:sw :e]]}
+   {:id "k" :row 0 :col 4
+    :links [[:w :sw]]}
+   {:id "l" :row 1 :col 3
+    :links [[:ne :se]]}
+   {:id "m" :row 2 :col 4
+    :links [[:nw :w]]}
+   {:id "n" :row 2 :col 3
+    :links [[:e :nw]]}])
